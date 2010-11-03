bed739e053bfca6cf938adb9a6441d16f013f26f
Piotr Gabryanczyk <piotrga@gmail.com>
2010-11-03 14:53:56 +0000

    onlyElementOf

--- src/main/java/shouldmatchers/Sugar.java
+++ src/main/java/shouldmatchers/Sugar.java
@@ -5,6 +5,11 @@ import java.util.List;
 import static shouldmatchers.ShouldMatchers.the;
 
 public class Sugar {
+    public static <T> T onlyElementOf(List<T> list) {
+        the(list).shouldHaveOneElementOnly();
+        return list.get(0);
+    }
+
     public static <T> T firstElementOf(List<T> list) { return nthElementOf(list, 1); }
     public static <T> T secondElementOf(List<T> list) { return nthElementOf(list, 2); }
 
